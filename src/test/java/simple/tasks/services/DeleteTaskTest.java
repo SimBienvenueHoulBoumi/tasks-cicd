89973e7759840914af6da87250de4d03a6502604
SimBienvenueHoulBoumi <brhulla@gmail.com>
2025-06-20 17:37:27 +0200

    correct code with add jpa

--- src/test/java/simple/tasks/services/DeleteTaskTest.java
+++ src/test/java/simple/tasks/services/DeleteTaskTest.java
@@ -2,7 +2,7 @@ package simple.tasks.services;
 
 import main.java.simple.tasks.exceptions.*;
 
-import jpa.TasksRepository;
+import main.java.simple.tasks.jpa.*;
 import models.Tasks;
 import org.junit.jupiter.api.Test;
 
